67ab1226a4b75c494ef6c50a4c791ed97e52e7d0
yutonihei <yutonihei@toshsMBP.local>
2018-09-23 16:22:44 +0900

    change header

---
+++ imports/ui/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+	return (
+      <header className="header">
+        <h1>Simple Memo on Heroku</h1>
+      </header>
+		);
+    }
+}
